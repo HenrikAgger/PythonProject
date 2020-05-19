77e12c08435e4a067c56c636da2d2d2c0047b5fa
unknown <cph-ha104@cphbusiness.dk>
2020-05-19 19:27:54 +0200

    some text added

--- df_handler.py
+++ df_handler.py
@@ -4,23 +4,27 @@ from sklearn import preprocessing
 import diabetes_corr as dc
 import matplotlib.pyplot as plt
 
+# Data Cleaning
+# 1) There are no duplicate or irelevant observations
+# 2) There are no same category occuring multiple times
+# 3) There are no missing or null data.
+# 4) There are some unexpected outliers. View: def zero_values(df):
 
 def read_csv(path):
-    # Read the file
     df = pd.read_csv(path)
     df.dropna() # No NaN values
     return df
 
 
-def negative_positive(df): # inspired by titanic 
+def negative_positive(df): # inspired by the titanic assignment
     # convert class tested_negative or tested_positive to 0 or 1
     label_enc = preprocessing.LabelEncoder()
     df['Class'] = label_enc.fit_transform(df['Class'].astype(str))
     return df
 
-
+# 4) There are some zero values
 def zero_values(df):
-    print("Total number of rows and columns before cleaning = {0} ".format(df.shape))
+    print("Total number of rows and columns BEFORE cleaning = {0} ".format(df.shape))
     print("Number of rows missing - Pregnancies = {0}".format(len(df.loc[df['Pregnancies'] == 0])))
     print("Number of rows missing - Glucose = {0}".format(len(df.loc[df['Glucose'] == 0])))
     print("Number of rows missing - Bloodpressure  = {0}".format(len(df.loc[df['Bloodpressure'] == 0])))
@@ -35,9 +39,10 @@ def zero_values(df):
 def mean_val_insulin(df):
     df.value = df.groupby('Class')['Insulin'].apply(lambda x: x.fillna(x.mean()))
     df.value = df.value.fillna(df.value.mean())
-    print(df.shape)
+    #print(df.shape)
 
 
+    # Fewer data but no zero values in the df. 
 def new_dataframe(df):
     # Because 227 patients did had 0 skinthickness, we thought it would be best to remove this columns from the df,
     # Neither did it correlate much with other columns in the df
@@ -52,10 +57,9 @@ def new_dataframe(df):
     (df['Diabetes_pedigree_function'] > 0) & 
     (df['Age'] > 0)
     ]
-    print("Total number of rows and columns after cleaning = {0} ".format(new_df.shape))
+    print("Total number of rows and columns AFTER cleaning = {0} ".format(new_df.shape))
     dc.plot_corr(new_df) 
-    new_df.corr()   
-    # Fewer data but no zero values in the df. 
+    new_df.corr()
     # Note fx. that glycose correlates 49% with the class(diabetes)
     
     new_df.to_csv('newfile.csv', index=False)
