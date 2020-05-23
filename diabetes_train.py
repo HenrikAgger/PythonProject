aa15e9f2a8932a15cf3871ad7fe6ec8a92e5766d
unknown <cph-ha104@cphbusiness.dk>
2020-05-23 09:13:54 +0200

    few changes

--- diabetes_train.py
+++ diabetes_train.py
@@ -5,17 +5,18 @@ from pandas import DataFrame as df
 import matplotlib.pyplot as plt
 
 
-def train_data(df):
-    feature_col_names = ['Pregnancies','Glucose','Bloodpressure','Skinthickness','Insulin','Bodymass','Diabetes_pedigree_function','Age']            
+def train_data(newdf):
+    feature_col_names = ['Pregnancies', 'Glucose', 'Bloodpressure', 'Insulin', 'Bodymass', 'Diabetes_pedigree_function', 'Age']
     predicted_class_names = ['Class']
 
-    X = df[feature_col_names].values 
-    y = df[predicted_class_names].values 
+    X = newdf[feature_col_names].values
+    y = newdf[predicted_class_names].values
 
     split_test_size = 0.3
 
-    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = split_test_size,random_state=42)
-    return X_train, X_test, y_train,y_test
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=split_test_size, random_state=42)
+    return X_train, X_test, y_train, y_test
 
 
 def naive_bayes(X_train, y_train):
@@ -25,14 +26,11 @@ def naive_bayes(X_train, y_train):
     return nb_model
 
 
-def models_acuracy(X_train,nb_model):
+def models_acuracy(X_train, nb_model):
     nb_predict_train = nb_model.predict(X_train)
     return nb_predict_train
-    
+
 
 def models_acuracy_X_Test(X_test, nb_model):
-    nb_predict_test=nb_model.predict(X_test)
+    nb_predict_test = nb_model.predict(X_test)
     return nb_predict_test
-
-
-    
